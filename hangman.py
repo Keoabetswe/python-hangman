37005e02bf961b98e5c75b4c3f891434180ae636
Keoabetswe <keo.nthite@iress.com>
2021-08-13 17:26:05 +0200

    fix blank[i+1] typo to blanks[i+1]

--- hangman.py
+++ hangman.py
@@ -56,7 +56,7 @@ def displayBoard(missedLetters, correctLetters, secretWord):
 
     for i in range(len(secretWord)): #replace blanks with correctly guessed letters.
         if secretWord[i] in correctLetters:
-            blanks = blanks[:i] + secretWord[i] + blank[i+1:]
+            blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
 
     for letter in blanks: #show the secret word with spaces in between each letter.
         print(letter, end=' ')
